cf1691e25b93e1fb785f935bd8cc2d04788d2212
simajune <tejayjung@gmail.com>
2018-01-22 04:05:07 +0900

    Add Asset files

--- JoyfulChurch/JoyfulChurch/MainViewController.swift
+++ JoyfulChurch/JoyfulChurch/MainViewController.swift
@@ -13,7 +13,6 @@ class MainViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,15 +20,7 @@ class MainViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func menuBtnAction(_ sender: UIButton) {
     }
-    */
-
+    
 }
